78c971d83ea9bf65b375e9f46b4cd4a46066250b
arturstaation <arturmackenzie12@gmail.com>
2025-01-06 20:32:41 -0300

    subindo grande att para gh pages

--- src/enums/Estados.ts
+++
@@ -1,8 +0,0 @@
-export enum Estados {
-    NaoIniciado = "Não Iniciado",
-    EmProgresso = "Em Progresso",
-    Pausado = "Pausado",
-    Deletado = "Deletado",
-    Finalizado = "Finalizado",
-    Aboratdo = "Abortado",
-}
